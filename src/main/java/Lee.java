0178777395f05338fbf10cad473e02b9895c788b
dingZvel <zhezhe0214@gmail.com>
2025-01-23 21:17:46 +0800

    [Level-1] Echo

--- src/main/java/Lee.java
+++ src/main/java/Lee.java
@@ -1,9 +1,25 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Lee {
+    private static final String horLine = "____________________________________________________________\n";
+    private static final String greets = "Hello! I'm Lee.\n"
+            + "What can I do for you?\n";
+    private static final String exits = "Bye. Hope to see you again soon!\n";
+
+    private static void echo(String command) {
+        System.out.println(horLine + command + "\n" + horLine);
+    }
+
     public static void main(String[] args) {
-        String horLine = "____________________________________________________________\n";
-        String greets = "Hello! I'm Lee.\n"
-                + "What can I do for you?\n";
-        String exits = "Bye. Hope to see you again soon!\n";
-        System.out.println(horLine + greets + horLine + exits + horLine);
+        System.out.println(horLine + greets + horLine);
+        Scanner sc = new Scanner(System.in);
+        String cmd = sc.nextLine();
+        while (!cmd.equals("bye")) {
+            echo(cmd);
+            cmd = sc.nextLine();
+        }
+        System.out.println(horLine + exits + horLine);
+        sc.close();
     }
 }
