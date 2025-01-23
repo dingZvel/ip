5e3ba3451e4b4374356e258f0c7d0291c773d01b
dingZvel <zhezhe0214@gmail.com>
2025-01-23 22:23:51 +0800

    [Level 3] Mark as Done

--- src/main/java/Lee.java
+++ src/main/java/Lee.java
@@ -6,31 +6,52 @@ public class Lee {
     private static final String greets = "Hello! I'm Lee.\n"
             + "What can I do for you?\n";
     private static final String exits = "Bye. Hope to see you again soon!\n";
-    private static String[] tasks = new String[100];
+    private static final Task[] tasks = new Task[100];
+//    private static String[] tasks = new String[100];
+//    private static boolean[] taskStatus = new boolean[100];
     private static int numOfTasks = 0;
 
     private static void analyze(String command) {
         System.out.print(horLine);
+        String[] cmds = command.split(" ");
+        String first = cmds[0];
         if (command.equals("list")) {
             listTasks();
+        }
+        else if (first.equals("mark")) {
+            mark(cmds[1], true);
+        }
+        else if (first.equals("unmark")) {
+            mark(cmds[1], false);
         }
         else {
             addTasks(command);
         }
-//        System.out.println(command + "\n" + horLine);
-    }
-
-    private static void listTasks(){
-        for (int i = 0; i < numOfTasks; i ++){
-            System.out.format("%d. " + tasks[i] + "\n", i + 1);
-        }
         System.out.println(horLine);
     }
 
+    private static void listTasks(){
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < numOfTasks; i ++){
+            System.out.format("%d." + showTask(i), i + 1);
+        }
+    }
+
     private static void addTasks(String task) {
-        tasks[numOfTasks] = task;
+        tasks[numOfTasks] = new Task(task);
         numOfTasks ++;
-        System.out.format("added: %s\n" + horLine + "\n", task);
+        System.out.format("added: %s\n", task);
+    }
+
+    private static String showTask(int index) {
+        return tasks[index].toString() + "\n";
+    }
+
+    private static void mark(String num, boolean b){
+        int index = Integer.parseInt(num) - 1;
+        tasks[index].isDone = b;
+        System.out.format("%s I've marked this task as %s:\n"
+                + showTask(index), b ? "Nice!" : "OK,", b ? "done" : "not done yet");
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] " + this.description, this.getStatusIcon());
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+}
