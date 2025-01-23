62c3f26a0f60951b76c54d30199ee89ee57bc318
dingZvel <zhezhe0214@gmail.com>
2025-01-23 21:41:48 +0800

    [Level 2] Add, List

--- src/main/java/Lee.java
+++ src/main/java/Lee.java
@@ -6,9 +6,31 @@ public class Lee {
     private static final String greets = "Hello! I'm Lee.\n"
             + "What can I do for you?\n";
     private static final String exits = "Bye. Hope to see you again soon!\n";
+    private static String[] tasks = new String[100];
+    private static int numOfTasks = 0;
 
-    private static void echo(String command) {
-        System.out.println(horLine + command + "\n" + horLine);
+    private static void analyze(String command) {
+        System.out.print(horLine);
+        if (command.equals("list")) {
+            listTasks();
+        }
+        else {
+            addTasks(command);
+        }
+//        System.out.println(command + "\n" + horLine);
+    }
+
+    private static void listTasks(){
+        for (int i = 0; i < numOfTasks; i ++){
+            System.out.format("%d. " + tasks[i] + "\n", i + 1);
+        }
+        System.out.println(horLine);
+    }
+
+    private static void addTasks(String task) {
+        tasks[numOfTasks] = task;
+        numOfTasks ++;
+        System.out.format("added: %s\n" + horLine + "\n", task);
     }
 
     public static void main(String[] args) {
@@ -16,10 +38,10 @@ public class Lee {
         Scanner sc = new Scanner(System.in);
         String cmd = sc.nextLine();
         while (!cmd.equals("bye")) {
-            echo(cmd);
+            analyze(cmd);
             cmd = sc.nextLine();
         }
-        System.out.println(horLine + exits + horLine);
+        System.out.print(horLine + exits + horLine);
         sc.close();
     }
 }
